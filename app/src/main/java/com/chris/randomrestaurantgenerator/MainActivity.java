b5295bc633ba41bbf6de3191a8e97bab2bedb127
Carlos Esponda <cfe160030@utdallas.edu>
2017-03-05 10:06:22 -0600

    Removed filters from side nav

--- app/src/main/java/com/chris/randomrestaurantgenerator/MainActivity.java
+++ app/src/main/java/com/chris/randomrestaurantgenerator/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void addDrawerItems(){
 
-        String[] thing = {"Likes","Dislikes", "Filters"};
+        String[] thing = {"Likes","Dislikes"};
         mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, thing);
 
         mDrawerList.setAdapter(mAdapter);
